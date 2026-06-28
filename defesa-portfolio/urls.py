bc40d9476cf19041dfe2f4b2a741b6ff38306fc5
lucas-foquim-a22406944 <foquimlucas@gmail.com>
2026-06-28 19:30:12 +0000

    Defesa Django

--- defesa-portfolio/urls.py
+++ defesa-portfolio/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('biblioteca/', include('biblioteca.urls')),
     path('escola/', include('escola.urls')),
